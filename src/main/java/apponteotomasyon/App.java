125b5523ecd5af113614ce20569fb388778aeb7a
Usame Muhammed <ussama14863@gmail.com>
2024-09-23 09:24:54 +0300

    first commit

---
+++ src/main/java/apponteotomasyon/App.java
@@ -0,0 +1,13 @@
+package apponteotomasyon;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
